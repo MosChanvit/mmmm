d85c6a3e37a8a93acce9f2726d30fb4fb34f6741
MosChanvit <kungmoszaza@gmail.com>
2016-10-22 20:35:00 +0700

    Initial commit

---
+++ app/src/main/java/com/example/baihuay/huay.java
@@ -0,0 +1,13 @@
+package com.example.baihuay;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class huay extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.huay);
+    }
+}
